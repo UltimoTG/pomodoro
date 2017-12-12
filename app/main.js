b6f4649e67a511a98cd1a34d0058bc7ad4059588
Tashi Gurung <tashigurung@gmail.com>
2017-12-11 22:42:20 -0500

    Set up Setting and FAQ pages

--- app/main.js
+++ app/main.js
@@ -14,8 +14,50 @@ app.on('ready', function() {
   });
   appWindow.loadURL('file://' + __dirname + '/templates/timer.html');
 
+  // FAQ window
+  faqWindow = new BrowserWindow({
+      width:700,
+      height:450,
+      center:true,
+      transparent: true,
+      show: false,
+      frame: false
+  });
+  faqWindow.loadURL('file://' + __dirname + '/templates/faq.html');
+
+  // Settings window
+  settingsWindow = new BrowserWindow({
+      width:700,
+      height:450,
+      center:true,
+      transparent: true,
+      show: false,
+      frame: false
+  });
+  settingsWindow.loadURL('file://' + __dirname + '/templates/settings.html');
+
   // display main window once ready to show
   appWindow.once('ready-to-show', function() {
     appWindow.show();
+  });
+
+  ipc.on('openFAQ', function(event, arg){
+    event.returnValue='';
+    faqWindow.show();
+  });
+
+  ipc.on('closeFAQ', function(event, arg){
+    event.returnValue='';
+    faqWindow.hide();
+  });
+
+  ipc.on('openSettings', function(event, arg){
+    event.returnValue='';
+    settingsWindow.show();
+  });
+
+  ipc.on('closeSettings', function(event, arg){
+    event.returnValue='';
+    settingsWindow.hide();
   });
 });
